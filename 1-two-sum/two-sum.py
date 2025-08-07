5c05e2cbbbab404e84bcef41ac3bd4a7ba26b63a
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-08-07 20:52:18 +0530

    Time: 1 ms (65.52%) | Memory: 12.9 MB (97.89%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        indices = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            indices[n] = i
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+            
+        
+
+            
+
+        
