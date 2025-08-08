3aee257a46069a670bf136d6a2480142fc6f40f1
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-08-08 15:18:06 +0530

    Time: 27 ms (90.45%) | Memory: 20.2 MB (84.26%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,29 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        prefix=[]
-        i=0
-        while i<len(nums):
-            if i==0:
-                prefix.append(1)
-            else:
-                prefix.append(nums[i-1]*prefix[-1])
-            i+=1
-
-        suffix=[]
-        j=len(nums)-1
-        while j>=0:
-            if j==(len(nums)-1):
-                suffix.append(1)
-            else:
-                suffix.append(nums[j+1]*suffix[-1])
-            j-=1
-
-        suf=[]
-        for i in range(len(suffix)-1,-1,-1):
-            suf.append(suffix[i])
-
-        res=[]
-        for i,j in zip(prefix,suf):
-            res.append(i*j)
+        res = [1]*len(nums)
+        prefix=1
+        for i in range(len(nums)):
+            res[i]=prefix
+            prefix*=nums[i]
+        suffix=1
+        for j in range(len(nums)-1,-1,-1):
+            res[j]*=suffix
+            suffix*=nums[j]
         return res
