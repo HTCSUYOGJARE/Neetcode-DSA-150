911c334ef8b83668dfe95aa2da5477fe27440db2
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-08-10 15:12:16 +0530

    Time: 2299 ms (23.11%) | Memory: 12.9 MB (36.58%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        S1=defaultdict(int)
+        for i in s1:
+            S1[i]+=1
+        for i in range(len(s2)-len(s1)+1):
+            if s2[i] not in S1:
+                continue
+            else:
+                d=defaultdict(int)
+                for k in range(i,i+len(s1)):
+                    d[s2[k]]+=1
+                if d==S1:
+                    return True
+                else:
+                    continue
+        return False
