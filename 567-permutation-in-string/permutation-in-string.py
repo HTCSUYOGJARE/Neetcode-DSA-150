290d9fdab46a39660e29060dff93d12bea040e81
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-08-10 15:39:27 +0530

    Time: 2381 ms (22.56%) | Memory: 12.9 MB (36.58%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -8,15 +8,15 @@ class Solution(object):
         S1=defaultdict(int)
         for i in s1:
             S1[i]+=1
-        for i in range(len(s2)-len(s1)+1):
+        i=0
+        while i<(len(s2)-len(s1)+1):
             if s2[i] not in S1:
-                continue
+                i+=1
             else:
                 d=defaultdict(int)
                 for k in range(i,i+len(s1)):
                     d[s2[k]]+=1
                 if d==S1:
                     return True
-                else:
-                    continue
+                i+=1
         return False
