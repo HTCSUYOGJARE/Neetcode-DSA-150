983563b3ff834ba8b7965ad03accc9b3ad685e97
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-08-10 16:18:47 +0530

    Time: 28 ms (60.87%) | Memory: 12.8 MB (63.02%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,18 +5,33 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        S1=defaultdict(int)
-        for i in s1:
-            S1[i]+=1
-        i=0
-        while i<(len(s2)-len(s1)+1):
-            if s2[i] not in S1:
-                i+=1
-            else:
-                d=defaultdict(int)
-                for k in range(i,i+len(s1)):
-                    d[s2[k]]+=1
-                if d==S1:
-                    return True
-                i+=1
-        return False
+        if len(s1)>len(s2):
+            return False
+        s1cnt=[0]*26
+        s2cnt=[0]*26
+        for i in range(len(s1)):
+            s1cnt[ord(s1[i])-ord('a')]+=1
+            s2cnt[ord(s2[i])-ord('a')]+=1
+        matches=0
+        for i in range(26):
+            matches+=(1 if s1cnt[i]==s2cnt[i] else 0)
+        l=0
+        for r in range(len(s1),len(s2)):
+            if matches==26:
+                return True
+
+            index=ord(s2[r])-ord("a")
+            s2cnt[index]+=1
+            if s1cnt[index]==s2cnt[index]:
+                matches+=1
+            elif s1cnt[index]+1==s2cnt[index]:
+                matches-=1
+
+            index=ord(s2[l])-ord("a")
+            s2cnt[index]-=1
+            if s1cnt[index]==s2cnt[index]:
+                matches+=1
+            elif s1cnt[index]-1==s2cnt[index]:
+                matches-=1
+            l+=1
+        return matches==26
