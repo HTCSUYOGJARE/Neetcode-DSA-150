875b397269da984bbae8a6cdf8b7c3759435b8b3
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-08-19 17:55:27 +0530

    Time: 105 ms (12.31%) | Memory: 22.8 MB (42.27%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # d={}
+        # for j in points:
+        #     d[tuple(j)] = (((j[0])**2)+((j[1])**2))**(1/2) # key in dict must me immutable and unhashable. so used tuple instead of list
+        # # now we have dictionary with point as key(tuple) and value as dist to origin
+        heap =[]
+        for j in points:
+            dist = (((j[0])**2)+((j[1])**2))**(1/2)
+            heapq.heappush(heap,[dist,j])
+        # this gets us the min heap based on the value that is dist. to origin
+        res=[]
+        for i in range(k):
+            point = heapq.heappop(heap)
+            res.append(point[1]) # key in dict was tsored as tuple so converted back to list
+        return res
