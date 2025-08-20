b496aa23e447f4a9d4fd7aaebd7d296f5a39992b
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-08-20 18:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.66%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,8 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
+        res,sol=[],[]
+        def backtrack(i):
+            if i==len(nums):
+                res.append(sol[:])
+                return
+            # keep it empty
+            backtrack(i+1)
 
-        for num in nums:
-            res += [subset + [num] for subset in res]
-
+            #fill with value
+            sol.append(nums[i])
+            backtrack(i+1)
+            sol.pop()
+        backtrack(0)
         return res
