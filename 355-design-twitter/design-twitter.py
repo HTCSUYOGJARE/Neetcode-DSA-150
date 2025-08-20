9c7c3ffb2784566f6dc4c55c4cb6aac87bb9b6cc
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-08-20 11:34:41 +0530

    Time: 13 ms (61.98%) | Memory: 27.8 MB (17.43%) - LeetSync

---
+++ 355-design-twitter/design-twitter.py
@@ -0,0 +1,38 @@
+class Twitter:
+
+    def __init__(self):
+        self.follow_dict = defaultdict(set) # store the userid and set of its followees(whom the user follows)
+        self.tweets_dict = defaultdict(list) # stores user id and list of its TweetIDs
+        self.time_stamp=0
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.follow_dict[userId].add(userId)
+        self.tweets_dict[userId].append([self.time_stamp,tweetId])
+        self.time_stamp+=1
+    def getNewsFeed(self, userId: int) -> List[int]:
+        tweets_list =[]
+        for i in self.follow_dict[userId]:
+            tweets_list.extend(self.tweets_dict[i])
+        max_heap = [[-x[0],x[1]] for x in tweets_list]
+        heapq.heapify(max_heap)
+        k=0
+        l= len(max_heap)
+        res=[]
+        while k<10 and k<l:
+            latest_tweetid = heapq.heappop(max_heap)
+            res.append(latest_tweetid[1])
+            k+=1
+        return res
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follow_dict[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.follow_dict[followerId].discard(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
