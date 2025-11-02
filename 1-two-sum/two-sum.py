a89f03a84c4a6ee2fb04f588f17aa27bc41520dd
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-11-02 23:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (47.48%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,17 +1,15 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        indices = {}  # val -> index
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i in range(len(nums)):
+            d[nums[i]] = i
 
-        for i, n in enumerate(nums):
-            indices[n] = i
-
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in indices and indices[diff] != i:
-                return [i, indices[diff]]
-            
-        
-
-            
-
-        
+        for idx,val in enumerate(nums):
+            if target-val in d and idx!=d[target-val]:
+                return [idx,d[target-val]]
+            continue
