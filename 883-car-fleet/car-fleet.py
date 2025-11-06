3026e42c1bd37d3bba642a462f3029a99a333e2d
HTCSUYOGJARE <htcsuyogjare@gmail.com>
2025-11-06 23:53:05 +0530

    Time: 222 ms (36.06%) | Memory: 48.7 MB (5.09%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,14 +1,13 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        d={}
-        for i in range(len(position)):
-            d[position[i]]=(target-position[i])/speed[i]
-        D = dict(sorted(d.items(),key=lambda x: x[0],reverse=True))
-        time_stack=deque()
-        for j in D:
-            if time_stack:
-                if  D[j]>time_stack[-1]:
-                    time_stack.append(D[j])
-            else:
-                time_stack.append(D[j])
-        return len(time_stack)
+        pos_time = {pos:(target-pos)/speed for pos,speed in zip(position,speed)}
+        sort_pos_time = sorted(pos_time.items(),key = lambda x:x[0],reverse=True)
+        times = [v for k,v in sort_pos_time]
+        stack = deque()
+        stack.append(times[0])
+        for t in times:
+            if stack and t>stack[-1]:
+                stack.append(t)
+        return len(stack)
+
+        
